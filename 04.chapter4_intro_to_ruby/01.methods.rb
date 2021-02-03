75fae3271469674334fdb48990e120213b85960c
Nathan Worden <worden.nathan@gmail.com>
2021-02-03 14:19:24 -0800

    add methods

---
+++ 04.chapter4_intro_to_ruby/01.methods.rb
@@ -0,0 +1,5 @@
+def say_goodnight(name)
+  result = 'Good night, ' + name return result
+  end
+  # Time for bed...
+  puts say_goodnight('Mary-Ellen') # => 'Goodnight, Mary-Ellen' puts say_goodnight('John-Boy') # => 'Goodnight, John-Boy'
